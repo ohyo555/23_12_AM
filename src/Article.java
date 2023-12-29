1e7ccff520a2a9fcf584dc69b95d25320863ab01
ohyo555 <555gywn@gmail.com>
2023-12-29 13:16:47 +0900

    작업2. 게시글 작성 기능 추가

---
+++ src/Article.java
@@ -0,0 +1,36 @@
+
+public class Article {
+	private int num;
+	private String title;
+	private String content;
+	
+	public Article(int num, String title, String content) {
+		this.num = num;
+		this.title = title;
+		this.content = content;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
