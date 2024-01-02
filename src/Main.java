9104b4f215583a826e784aaa0045eb56db0670b0
ohyo555 <555gywn@gmail.com>
2024-01-02 17:46:19 +0900

    작업12-2, 비밀번호 확인 수정

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,7 @@ public class Main {
 
 		int lastArticleId = 3;
 		int lastMemberId = 0;
+
 		while (true) {
 			System.out.print("명령어 > ");
 			String cmd = sc.nextLine().trim();
@@ -32,7 +33,6 @@ public class Main {
 				System.out.println("==회원 가입==");
 				int id = lastMemberId + 1;
 				String regDate = Util.getNowDate_TimeStr();
-				
 				String loginId = null;
 				while (true) {
 					System.out.print("로그인 아이디 : ");
@@ -43,16 +43,21 @@ public class Main {
 					}
 					break;
 				}
-				System.out.print("로그인 비밀번호 : ");
-				String loginPw = sc.nextLine();
-				
-				System.out.print("로그인 비밀번호 재입력: ");
-				String loginPw_check = sc.nextLine();
-				
-				if (loginPw != loginPw_check) {
-					System.out.println("비밀번호가 제대로 입력되지 않았습니다.");
+				String loginPw = null;
+
+				while (true) {
+					System.out.print("로그인 비밀번호 : ");
+					loginPw = sc.nextLine();
+					System.out.print("로그인 비밀번호 확인: ");
+					String loginPwConfirm = sc.nextLine();
+
+					if (loginPw.equals(loginPwConfirm) == false) {
+						System.out.println("비밀번호 다시 확인해");
+						continue;
+					}
+					break;
 				}
-				
+
 				System.out.print("이름 : ");
 				String name = sc.nextLine();
 
@@ -213,6 +218,7 @@ public class Main {
 				return false;
 			}
 		}
+
 		return true;
 	}
 
